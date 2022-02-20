8a96336e73b73e89040e6152b34faaeb156c9c21
Oscar Bucio <oscarbucio2001@gmail.com>
2022-02-20 15:11:23 -0600

    First graphic done

--- programa1.py
+++ programa1.py
@@ -1,14 +1,21 @@
 #Variables
 from random import randint
+from timeit import default_timer
+import matplotlib.pyplot as plt
 i = 0
 j = 0
 decision = ""
 opcion = 0
-f = open("Programa1.txt", "w")
+nsimbolos = []
+ntotal = []
+contador = 0
 #Funciones
-def binarios(): 
-    cadenas = ['{', 'ε']
-    opcion = int(input("Introduzca el numero de hasta donde desea obtener el universo de cadenas\n"))
+
+def binarios(opcion): 
+    #cadenas = ['{', 'ε']
+    global contador
+    f = open("Programa1.txt", "w", encoding="utf-8")
+    f.write('Σ^* = ' + '{' + 'ε' + ',' + "\n")
     if opcion > 0 and opcion < 100: 
         for i in range(1, opcion + 1):
             for j in range(0, 2 ** i): #Recorrer y generar la cadena desde 0 hasta el numero especificado
@@ -21,64 +28,75 @@ def binarios():
                     #aux = zeros + aux
                     #function mode
                     aux = aux.zfill(i)
-                    cadenas.append(aux)
+                    contador = contador + len(aux)
+                    f.writelines(aux + ",")
+                    #cadenas.append(aux) esto es usando una lista (mucha memoria)
                 else: 
-                    cadenas.append(aux)
-        cadenas.append('}')
+                    f.writelines(aux + ",")
+                    contador = contador + len(aux)
+                    #cadenas.append(aux)
+            nsimbolos.append(contador)
+            contador = 0
+            f.write("\n\n")
+        #cadenas.append('}')
+        f.write('}')
+        f.close()
     else: 
         print("El numero está fuera del rango\n\n")
-    
-    #formateando
+    #formateando si se usara una lista
+    # i = 0
+    # for i in range(len(cadenas)):
+    #     f.write(cadenas[i] + "\n")
+    # f.close()
+
+
+def graficacion(opcion):
+    plt.clf()
     i = 0
-    for i in range(len(cadenas)):
-        f.write(cadenas[i] + "\n")
-    f.close()
+    for i in range(1, opcion + 1): #llenando la lista del eje x respecto a lo elegido por el usuario
+        ntotal.append(i)
 
+    puntosx = ntotal
+    puntosy = nsimbolos
+    plt.plot(puntosx, puntosy)
+    plt.xlabel("Numeros de cadenas")
+    plt.ylabel("Numeros de simbolos por cadena")
+    plt.show()
     
-    print(cadenas)
 
-def binariosRandom():
-    random = randint(1,10)
-    print(random)
-    cadenas = ['{', 'ε']
-    if random > 0 and random < 100: 
-        for i in range(1, random + 1):
-            for j in range(0, 2 ** i): #Recorrer y generar la cadena desde 0 hasta el numero especificado
-                #Conversion a binario
-                aux = str(bin(j))[2:]
-                tam = len(aux)
-                if tam < i: 
-                    #modo manual
-                    #zeros = '0' * i
-                    #aux = zeros + aux
-                    #function mode
-                    aux = aux.zfill(i)
-                    cadenas.append(aux)
-                else: 
-                    cadenas.append(aux)
-        cadenas.append('}')
-    else: 
-        print("El numero está fuera del rango\n\n")
-
-    i = 0
-    for i in cadenas:
-        f.write(i + "\n")
-    f.close()
-    print(cadenas)
-    
 
 #Menu 
 while decision != "no":
     print("Bienvenido a la calculadora de universos de cadenas binarias!")
-    decision = int(input("1. Introducir numero\n2. Número aleatorio entre 1-1000\n3. Salir\n"))
+    decision = int(input("1. Introducir numero\n2. Número aleatorio entre 1-1000\n3. Salir\n\n"))
+
     if decision == 1:
-        binarios()
+        opcion = int(input("Introduzca el numero de hasta donde desea obtener el universo de cadenas: "))
+        #Mandando a llamar a la funcion y midiendo el tiempo
+        inicio = default_timer()
+        binarios(opcion)
+        fin = default_timer()
+        tiempo = fin - inicio
+        print(f"El tiempo de ejecucion fue: {tiempo} segundos\n")
+        print("Revise el archivo llamado Programa1.txt\n\n")
+        graficacion(opcion)
+
     elif decision == 2:
-        binariosRandom()
+        random = randint(1,1000)
+        inicio = default_timer()
+        binarios(random)
+        fin = default_timer
+        tiempo = fin - inicio
+        print(f"El tiempo de ejecucion fue: {tiempo} segundos\n")
+        print("Revise el archivo llamado Programa1.txt\n\n")
+        graficacion()
+
     elif decision == 3: 
+        print("Hasta pronto!")
         break
     else:
         print("INGRESE UNA OPCION VALIDA \n\n")
-    decision = input("Desea calcular de otro numero?\n[si/no] \n\n").lower()
+    decision = input("Desea calcular de otro numero? [si/no]: ").lower()
     if decision != 'si' and decision != 'no':
+        print("Hasta pronto!")
         break
