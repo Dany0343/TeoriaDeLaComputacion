6bb57f2533fbeb4cd7c25e13161d3781c33c66b4
Oscar Bucio <oscarbucio2001@gmail.com>
2022-02-20 20:11:23 -0600

    Some changes...

--- programa1.py
+++ programa1.py
@@ -1,22 +1,27 @@
 #Variables
-from random import randint
+from random import randint, random
 from timeit import default_timer
 import matplotlib.pyplot as plt
-i = 0
+import math
+
+i = 0 #Iteradores
 j = 0
-decision = ""
-opcion = 0
-nsimbolos = []
-ntotal = []
+decision = "" #Seleccionar el tipo de entrada. Automatica o manual
+opcion = 0 #Dato entregado a binarios para proceder a generar la función
+nsimbolos = [] #Lista que guarda el numero total de simbolos de cada cadena
+ntotal = [] #Lista que guarda los numeros desde el 1 hasta la elección del usuario, haya sido automatica o manual
+unos = [] #Lista que guarda el numero total de unos de cada cadena
 contador = 0
+contadorUnos = 0
 #Funciones
 
 def binarios(opcion): 
     #cadenas = ['{', 'ε']
     global contador
+    global contadorUnos
     f = open("Programa1.txt", "w", encoding="utf-8")
     f.write('Σ^* = ' + '{' + 'ε' + ',' + "\n")
-    if opcion > 0 and opcion < 100: 
+    if opcion > 0 and opcion < 1000: 
         for i in range(1, opcion + 1):
             for j in range(0, 2 ** i): #Recorrer y generar la cadena desde 0 hasta el numero especificado
                 #Conversion a binario
@@ -29,21 +34,25 @@ def binarios(opcion):
                     #function mode
                     aux = aux.zfill(i)
                     contador = contador + len(aux)
+                    contadorUnos += aux.count('1')
                     f.writelines(aux + ",")
                     #cadenas.append(aux) esto es usando una lista (mucha memoria)
                 else: 
                     f.writelines(aux + ",")
+                    contadorUnos +=  aux.count('1')
                     contador = contador + len(aux)
                     #cadenas.append(aux)
             nsimbolos.append(contador)
+            unos.append(contadorUnos)
             contador = 0
+            contadorUnos = 0
             f.write("\n\n")
         #cadenas.append('}')
         f.write('}')
         f.close()
     else: 
         print("El numero está fuera del rango\n\n")
-    #formateando si se usara una lista
+    #formateando si se usara una lista too fucking much memory
     # i = 0
     # for i in range(len(cadenas)):
     #     f.write(cadenas[i] + "\n")
@@ -55,14 +64,25 @@ def graficacion(opcion):
     i = 0
     for i in range(1, opcion + 1): #llenando la lista del eje x respecto a lo elegido por el usuario
         ntotal.append(i)
-
+    #Primera gráfica, numero de cadenas y numero total de simbolos
     puntosx = ntotal
-    puntosy = nsimbolos
-    plt.plot(puntosx, puntosy)
-    plt.xlabel("Numeros de cadenas")
+    puntosy1 = nsimbolos
+    plt.plot(puntosx, puntosy1, color = 'r')
+    plt.xlabel("Numero de cadenas")
     plt.ylabel("Numeros de simbolos por cadena")
+    plt.title("Primera gráfica. Numero de cadena y numero total de simbolos")
+    plt.grid()
     plt.show()
-    
+
+
+    #Segunda gráfica, numero de cadenas y numero total de unos
+    puntosy2 = unos
+    plt.plot(puntosx, puntosy2, color = 'b')
+    plt.xlabel("Numero de cadena")
+    plt.ylabel("Numeros de unos por cadena")
+    plt.title("Segunda gráfica. Numero de cadena y numero total de unos")
+    plt.grid()
+    plt.show()
 
 
 #Menu 
@@ -71,25 +91,41 @@ while decision != "no":
     decision = int(input("1. Introducir numero\n2. Número aleatorio entre 1-1000\n3. Salir\n\n"))
 
     if decision == 1:
+        nsimbolos = []
+        ntotal = []
+        unos = []
         opcion = int(input("Introduzca el numero de hasta donde desea obtener el universo de cadenas: "))
         #Mandando a llamar a la funcion y midiendo el tiempo
         inicio = default_timer()
         binarios(opcion)
         fin = default_timer()
         tiempo = fin - inicio
-        print(f"El tiempo de ejecucion fue: {tiempo} segundos\n")
+        if tiempo > 120:
+            tiempo = tiempo / 60
+            print(f"El tiempo de ejecucion fue: {tiempo} minutos\n")
+        else:
+            print(f"El tiempo de ejecucion fue: {tiempo} segundos\n")
         print("Revise el archivo llamado Programa1.txt\n\n")
         graficacion(opcion)
 
     elif decision == 2:
-        random = randint(1,1000)
+        nsimbolos = []
+        ntotal = []
+        unos = []
+        opcion = randint(1,1000)
+        print(opcion)
+        #Mandando a llamar a la funcion y midiendo el tiempo
         inicio = default_timer()
-        binarios(random)
-        fin = default_timer
+        binarios(opcion)
+        fin = default_timer()
         tiempo = fin - inicio
-        print(f"El tiempo de ejecucion fue: {tiempo} segundos\n")
+        if tiempo > 120:
+            tiempo = tiempo / 60
+            print(f"El tiempo de ejecucion fue: {tiempo} minutos\n")
+        else:
+            print(f"El tiempo de ejecucion fue: {tiempo} segundos\n")
         print("Revise el archivo llamado Programa1.txt\n\n")
-        graficacion()
+        graficacion(opcion)
 
     elif decision == 3: 
         print("Hasta pronto!")
@@ -97,6 +133,6 @@ while decision != "no":
     else:
         print("INGRESE UNA OPCION VALIDA \n\n")
     decision = input("Desea calcular de otro numero? [si/no]: ").lower()
-    if decision != 'si' and decision != 'no':
+    if  decision != 'si':
         print("Hasta pronto!")
         break
