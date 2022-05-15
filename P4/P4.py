6be6a5c53e0ed0c28106f866ac763c4b38f36a0b
Oscar Bucio <oscarbucio2001@gmail.com>
2022-05-15 13:12:02 -0500

    Tamos de vuelta

---
+++ P4/P4.py
@@ -0,0 +1,33 @@
+import random
+from random import randint
+import time
+from turtle import *
+import turtle as tur
+
+# Programar el autómata finito determinístico que reconozca las palabras:
+
+# web, webpage, website, webmaster, ebay, page, site
+
+# 1. Diseñar el NFA.
+# 2. Realizar la conversión a DFA mostrando todo el proceso a través de los subconjuntos y tablas.
+# 3. El programa deberá de leer un archivo de texto, podría ser de una página web.
+# 4. El autómata deberá de identificar cada palabra reservada con el DFA, contarlas e indicar dónde las encontró (posición).
+# 5. En un archivo imprimir la evaluación del autómata por cada carácter que lea y cambio de estado, es decir, toda la historia del proceso.
+# 6. En otro archivo enumerar, contar y anotar donde están las palabras encontradas.
+# 7. Tener una opción para ver el autómata, es decir, hay que graficarlo.
+# 8. Incluir en el reporte el código fuente.
+
+#Funciones
+def run():
+    print("Hola mundo!")
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    run()
