ab2e4db2628d1a75f8ff63b7b81b03801013f59d
Oscar Bucio <oscarbucio2001@gmail.com>
2022-05-30 17:53:12 -0500

    P7 started

---
+++ P7/P7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#Librerias
+from enum import auto
+import random
+from random import randint
+import time
+import turtle
+
+#Funciones
+def maquina():
+    print("Hola mundo!")
+
+if __name__ == '__main__':
+    maquina()
