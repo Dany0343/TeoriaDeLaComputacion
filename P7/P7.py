76148a4d4d48f0b8de49b9bcd8dd52a634ed71e4
Oscar Bucio <oscarbucio2001@gmail.com>
2022-06-02 01:26:55 -0500

    P7 started

--- P7/P7.py
+++ P7/P7.py
@@ -7,8 +7,24 @@ import time
 import turtle
 
 #Funciones
+
+def generacion():
+    cadenaGenerada = []
+    numeroAleatorio = randint(1, 50) #Con este numero se sabrá la longitud total de la cadena a generar
+    print(f"El numero aleatorio resultante es: {numeroAleatorio}")
+    for i in range(1, numeroAleatorio + 1):
+        letraRandom = random.choice(['_', '*', '|', 'a', 'X']) #De esta función se sacarán aleatoriamente los simbolos
+        cadenaGenerada.append(letraRandom)
+
+    print(cadenaGenerada)
+
 def maquina():
-    print("Hola mundo!")
+    print("Bienvenido a una prueba de la pedagogical universal Turing machine, que disfrute su estancia.")
+    eleccion = int(input("1.Introducir cadena \n2.Generar cadena aleatoria \n"))
+    if eleccion == 1:
+        
+    elif eleccion == 2:
+        generacion()
 
 if __name__ == '__main__':
     maquina()
